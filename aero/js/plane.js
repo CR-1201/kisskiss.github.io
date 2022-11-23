54d218d7177596820f4b820f27255faa068c94f3
CR-1201 <112941971+CR-1201@users.noreply.github.com>
2022-11-23 18:23:38 +0800

    Delete aero directory

--- aero/js/plane.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(show, 200);
-});
-function show(){
-    const title = document.querySelector('.main-title');
-    title.classList.add('show');
-}
